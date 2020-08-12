825bf01446c982675e25c4a50601dcc9830eb947
KanKK <286454796@qq.com>
2020-08-13 01:19:57 +0800

    docs: 默认主题配置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,5 +4,37 @@ module.exports = {
   // 用于修改内部的 Webpack 配置
   configureWebpack: {
 
+  },
+  themeConfig: {
+    // 搜索框
+    search: false,
+    // 导航栏
+    nav: [
+      {
+        text: "指南",
+        link: "/guide/",
+      },
+      {
+        text: "手册",
+        link: "/handbook/",
+      },
+    ],
+    // 侧边栏
+    sidebar: [
+      {
+        title: '文章 1',
+        path: '/',
+      },
+      {
+        title: '文章 2',
+        path: '/'
+      }
+    ],
+    // 下一篇按钮
+    nextLinks: false,
+    // 上一篇按钮
+    prevLinks: false,
+    // 页面滚动
+    smoothScroll: true
   }
 }
